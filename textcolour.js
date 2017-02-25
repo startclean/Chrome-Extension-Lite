5e8959c9b3b0bf2ab8f22b6672780612c1d4ede2
Elliott Weaver <elliott.weaver12@gmail.com>
2017-02-25 15:35:51 +0000

    Update textcolour.js

--- textcolour.js
+++ textcolour.js
@@ -1,16 +1,14 @@
-chrome.storage.sync.get({"colourvalue": "#b5b5a4"}, colourCallback);
-var colourCallback = function(list) {
-var colour = list["colourvalue"];
-console.log(colour);
-document.body.style.color = colour;
-for (var i = 0; i < document.getElementsByClassName("title").length; i++) {
-    document.getElementsByClassName("title")[i].style.color = colour;
-    document.getElementsByClassName("title")[i].style.borderBottom = "1px solid " + colour;
+window.onload = function () {
+    chrome.storage.sync.get("colourvalue", function(list) {
+    var colour = list["colourvalue"];
+    console.log(colour);
+    document.body.style.color = colour;
+    for (var i = 0; i < document.getElementsByClassName("title").length; i++) {
+      document.getElementsByClassName("title")[i].style.color = colour;
+      document.getElementsByClassName("title")[i].style.borderBottom = "1px solid " + colour;
+    }
+    for (var i = 0; i < document.querySelectorAll("ul > li > a").length; i++) {
+      document.querySelectorAll("ul > li > a")[i].style.color = colour;
+    }
+  });
 }
-for (var i = 0; i < document.querySelectorAll("#topsites > li > a").length; i++) {
-    document.querySelectorAll("#topsites > li > a")[i].style.color = colour;
-}	
-for (var i = 0; i < document.querySelectorAll("ul > li > a").length; i++) {
-    document.querySelectorAll("ul > li > a")[i].style.color = colour;
-}
-};
