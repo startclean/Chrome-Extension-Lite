51f694c52c0f2de823ccbc7da14d71a14daf17a0
Elliott Weaver <elliott.weaver12@gmail.com>
2017-02-25 14:34:46 +0000

    Update topsites.js

--- topsites.js
+++ topsites.js
@@ -68,6 +68,13 @@ function listTopSites() { //get the top 5 sites from chrome
 	}
 	});
 }
-
+document.addEventListener("DOMContentLoaded", function() {
+	chrome.storage.sync.get({"colourvalue": "#b5b5a4"}, colourCallback);
+});
+var colourCallback = function(list) {
+var colour = list["colourvalue"];
+for (var i = 0; i < document.querySelectorAll("#topsites > li > a").length; i++) {
+    document.querySelectorAll("#topsites > li > a")[i].style.color = colour;
+}
 
 
