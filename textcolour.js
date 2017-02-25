ff48a4f065e921b9de4e923d95a79c5817c2ebf4
Elliott Weaver <elliott.weaver12@gmail.com>
2017-02-25 13:27:14 +0000

    Create textcolour.js

---
+++ textcolour.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+	chrome.storage.sync.get({"colourvalue": "#b5b5a4"}, colourCallback);
+});
+var colourCallback = function(list) {
+var colour = list["colourvalue"];
+console.log(colour);
+document.body.style.color = colour;
+for (var i = 0; i < document.getElementsByClassName("title").length; i++) {
+    document.getElementsByClassName("title")[i].style.color = colour;
+    document.getElementsByClassName("title")[i].style.borderBottom = "1px solid " + colour;
+}
+for (var i = 0; i < document.querySelectorAll("ul > li > a").length; i++) {
+    document.getElementsByClassName("ul > li > a")[i].style.color = colour;
+}
+
+};
