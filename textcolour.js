9003c6af67392e8e2fe955690ba9eaacdb3da4fe
Elliott Weaver <elliott.weaver12@gmail.com>
2017-02-25 14:27:15 +0000

    Update textcolour.js

--- textcolour.js
+++ textcolour.js
@@ -9,8 +9,10 @@ for (var i = 0; i < document.getElementsByClassName("title").length; i++) {
     document.getElementsByClassName("title")[i].style.color = colour;
     document.getElementsByClassName("title")[i].style.borderBottom = "1px solid " + colour;
 }
+for (var i = 0; i < document.querySelectorAll("#topsites > li > a").length; i++) {
+    document.querySelectorAll("#topsites > li > a")[i].style.color = colour;
+}	
 for (var i = 0; i < document.querySelectorAll("ul > li > a").length; i++) {
     document.querySelectorAll("ul > li > a")[i].style.color = colour;
 }
-
 };
